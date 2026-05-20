34698796bd836699d41d7f62158c4e61865e018e
Ibrahima Kalil Kante <ibrahimakalilkante302@gmail.com>
2026-05-20 23:29:26 +0000

    kalil comopetence

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../components/Hero';
+import Skills from '../components/Skills';
 
 const Home: React.FC = () => {
   return (
     <>
       <Hero />
+      <Skills />
     </>
   );
 };
